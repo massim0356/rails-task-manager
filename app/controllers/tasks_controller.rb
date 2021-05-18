68d84b7032c7a545b795c99751c0a9ccefcbad34
Massim Shah <massimshah@hotmail.com>
2021-05-18 12:52:43 +0900

    added routes

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,24 @@
+class TasksController < ApplicationController
+  def new
+    @tasks = Task.all
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
